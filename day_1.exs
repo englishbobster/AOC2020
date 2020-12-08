848b95b34b480006012f17cfe7e7ea99718136f7
Stuart Osborne <stuart.osborne@infinera.com>
2020-12-08 14:53:46 +0100

    Second star day1...ugly ugly.

--- day_1.exs
+++ day_1.exs
@@ -11,23 +11,53 @@ defmodule Day1 do
     process_expenses(expenses, {})
   end
 
-  defp process_expenses(_, {a,b}) when b != 0 do
+  defp process_expenses(_, {a, b}) when b != 0 do
     a * b
   end
   defp process_expenses([], results) do
     results
   end
   defp process_expenses([h | t], _) do
-    result = t |> Enum.find(0, fn val -> val + h == 2020 end)
+    result = t
+             |> Enum.find(0, fn val -> val + h == 2020 end)
     current_result = {h, result}
     process_expenses(t, current_result)
   end
 
+  def sum_expenses(expenses) do
+    sum_expenses(expenses, [])
+  end
+
+  defp sum_expenses([], results) do
+    results
+  end
+  defp sum_expenses([h | t], results) do
+    result = t
+             |> Enum.reduce([], fn val, acc -> acc ++ [{h, val, h + val}] end)
+    results = results ++ result
+    sum_expenses(t, results)
+  end
+
+  defp sum_total_single(expenses, {a, b, total}) do
+    expenses
+    |> Enum.map(fn val -> {a, b, val, total + val} end)
+  end
+  def sum_total(expenses, summed_expenses) do
+    {a, b, c, _} = summed_expenses
+                   |> Enum.map(fn se -> sum_total_single(expenses, se) end)
+                   |> Enum.flat_map(fn x -> x end)
+                   |> Enum.find(fn {_, _, _, total} -> total == 2020 end)
+
+    a * b * c
+  end
 end
 
 exp = Day1.read_expenses()
 
 #First star!!
-Day1.process_expenses(exp) |> IO.puts
+Day1.process_expenses(exp)
+|> IO.puts
 
 #Second star!!
+initial_sum = Day1.sum_expenses(exp)
+IO.puts(Day1.sum_total(exp, initial_sum))
