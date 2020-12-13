37556c8c889b476cdcae72084a8c500d3801e836
Stuart Osborne <stuart.osborne@infinera.com>
2020-12-13 22:37:09 +0100

    day4 wip...nearly there but bed is calling.

--- day_4.exs
+++ day_4.exs
@@ -1,4 +1,6 @@
 defmodule Day4 do
+  #  @optional_keys [:cid]
+  @allowed_eye_colours [:amb, :blue, :brn, :gry, :grn, :hzl, :oth]
 
   def load_passports() do
     passports = File.read!("./resources/passports.txt")
@@ -6,6 +8,18 @@ defmodule Day4 do
     |> String.trim()
     |> String.split("\n\n")
     |> Enum.map(fn entry -> process_entry(entry) end)
+  end
+
+  def get_mandatory_keys do
+    [
+      {:byr, &validate_byr/1},
+      {:iyr, &validate_iyr/1},
+      {:eyr, &validate_eyr/1},
+      {:hgt, &validate_height/1},
+      {:hcl, &validate_hair_colour/1},
+      {:ecl, &validate_eye_colour/1},
+      {:pid, &validate_passport_id/1}
+    ]
   end
 
   def process_entry(entry) do
@@ -29,16 +43,80 @@ defmodule Day4 do
   end
 
   def has_necessary_keys(keys) do
-    mandatory_keys = [:byr, :iyr, :eyr, :hgt, :hcl, :ecl, :pid]
-    optional_keys = [:cid]
-    mandatory_keys
+    get_mandatory_keys()
+    |> Keyword.keys
     |> Enum.reduce(true, fn mandatory_key, acc -> acc && Enum.member?(keys, mandatory_key)  end)
   end
 
-  def count_passports_with_valid_fields() do
+  def find_passports_with_correct_fields() do
     load_passports()
     |> Enum.filter(fn passport -> valid_passport_fields?(passport) end)
+  end
+
+  def validate_year_between(year_str, least, most) do
+    year = String.to_integer(year_str)
+    Integer.digits(year)
+    |> length() == 4 && (year >= least) && (year <= most)
+  end
+  def validate_byr(year_str) do
+    validate_year_between(year_str, 1920, 2002)
+  end
+  def validate_iyr(year_str) do
+    validate_year_between(year_str, 2010, 2020)
+  end
+  def validate_eyr(year_str) do
+    validate_year_between(year_str, 2020, 2030)
+  end
+
+  def validate_height(height_str) do
+    height_regex = ~r/(?<value>\d+)(?<unit>cm|in)/
+    case Regex.named_captures(height_regex, height_str) do
+      %{"value" => value, "unit" => unit} -> validate_height_value(value, unit)
+      _ -> false
+    end
+  end
+
+  def validate_height_value(value_str, unit) do
+    value = String.to_integer(value_str)
+    case unit do
+      "in" -> value >= 59 && value <= 76
+      "cm" -> value >= 150 && value <= 193
+      _ -> false
+    end
+  end
+
+  def validate_hair_colour(colour) do
+    hair_regex = ~r/^#[0-9a-fA-F]{6}$/
+    Regex.match?(hair_regex, colour)
+  end
+
+  def validate_eye_colour(colour) do
+    String.to_atom(colour) in @allowed_eye_colours
+  end
+
+  def validate_passport_id(id) do
+    pid_regex = ~r/^[0-9]{9}$/
+    Regex.match?(pid_regex, id)
+  end
+
+  def count_passports_with_valid_fields() do
+    find_passports_with_correct_fields()
     |> Enum.count()
+  end
+
+  def count_passports_with_valid_fields_and_values() do
+    find_passports_with_correct_fields()
+    |> Enum.map(fn passport -> validate_passport_values(passport) end)
+  end
+
+  def validate_passport_values(passport) do
+    get_mandatory_keys()
+    |> Enum.map(
+         fn {key, func} ->
+           val = Keyword.get(passport, key)
+           func.(val)
+         end
+       )
   end
 
 end
@@ -47,3 +125,4 @@ end
 IO.puts(Day4.count_passports_with_valid_fields())
 
 #Second star!!
+IO.puts(Day4.count_passports_with_valid_fields_and_values())
