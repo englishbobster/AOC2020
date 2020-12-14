ad4d8897ddb8d9db9b47ffdf60be6906d35eec55
Stuart Osborne <stuart.osborne@infinera.com>
2020-12-14 11:20:55 +0100

    Second star for day4.

--- day_4.exs
+++ day_4.exs
@@ -1,6 +1,6 @@
 defmodule Day4 do
   #  @optional_keys [:cid]
-  @allowed_eye_colours [:amb, :blue, :brn, :gry, :grn, :hzl, :oth]
+  @allowed_eye_colours [:amb, :blu, :brn, :gry, :grn, :hzl, :oth]
 
   def load_passports() do
     passports = File.read!("./resources/passports.txt")
@@ -107,6 +107,7 @@ defmodule Day4 do
   def count_passports_with_valid_fields_and_values() do
     find_passports_with_correct_fields()
     |> Enum.map(fn passport -> validate_passport_values(passport) end)
+    |> Enum.count(fn valid -> valid == true end)
   end
 
   def validate_passport_values(passport) do
@@ -114,11 +115,38 @@ defmodule Day4 do
     |> Enum.map(
          fn {key, func} ->
            val = Keyword.get(passport, key)
-           func.(val)
+           result = func.(val)
+           #IO.puts("Try validate #{key} result: #{result} for val: #{val}")
+           result
          end
        )
+    |> Enum.reduce(true, fn valid, acc -> acc && valid end)
   end
 
+end
+
+# with tests
+ExUnit.start()
+defmodule Day4.PassportTest do
+  use ExUnit.Case
+
+  test "count valid passports" do
+    pp1 = "eyr:1972 cid:100 hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926"
+    pp2 = "iyr:2019 hcl:#602927 eyr:1967 hgt:170cm ecl:grn pid:012533040 byr:1946"
+    pp3 = "hcl:dab227 iyr:2012 ecl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277"
+    pp4 = "hgt:59cm ecl:zzz eyr:2038 hcl:74454a iyr:2023 pid:3556412378 byr:2007"
+    pp5 = "pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980 hcl:#623a2f"
+    pp6 = "eyr:2029 ecl:blu cid:129 byr:1989 iyr:2014 pid:896056539 hcl:#a97842 hgt:165cm"
+    pp7 = "hcl:#888785 hgt:164cm byr:2001 iyr:2015 cid:88 pid:545766238 ecl:hzl eyr:2022"
+    pp8 = "iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719"
+    result = [pp1, pp2, pp3, pp4, pp5, pp6, pp7, pp8]
+             |> Enum.map(fn entry -> Day4.process_entry(entry) end)
+             |> Enum.filter(fn passport -> Day4.valid_passport_fields?(passport) end)
+             |> Enum.map(fn passport -> Day4.validate_passport_values(passport) end)
+             |> Enum.count(fn valid -> valid == true end)
+    assert result == 4
+
+  end
 end
 
 #First star!!
