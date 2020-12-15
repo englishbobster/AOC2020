061e061cea2477d61b2feb99d43111786acfb332
Stuart Osborne <stuart.osborne@infinera.com>
2020-12-15 22:53:33 +0100

    First star day 6.

--- day_6.exs
+++ day_6.exs
@@ -5,6 +5,16 @@ defmodule Day6 do
     questions
     |> String.trim()
     |> String.split("\n\n")
+    |> Enum.map(fn group -> String.split(group, "\n") end)
+  end
+
+  def sum_counts_for_groups() do
+    load_group_questions()
+    |> Enum.map(fn group_list -> Enum.join(group_list) end)
+    |> Enum.map(fn str -> String.graphemes(str) end)
+    |> Enum.map(fn grapheme_list -> Enum.uniq(grapheme_list) end)
+    |> Enum.map(fn uniq_list -> Enum.count(uniq_list) end)
+    |> Enum.sum()
   end
 
 end
@@ -21,7 +31,8 @@ defmodule Day6.Questions do
 end
 
 #First star!!
-IO.puts("first star goes here")
+Day6.sum_counts_for_groups()
+IO.puts(Day6.sum_counts_for_groups())
 
 #Second star!!
 IO.puts("second star goes here")
