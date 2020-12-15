fc52c718cee63d86ee87e452967d9add3d99ea6b
Stuart Osborne <stuart.osborne@infinera.com>
2020-12-15 19:22:26 +0100

    day 5 WIP: calculate column working.

--- day_5.exs
+++ day_5.exs
@@ -8,16 +8,26 @@ defmodule Day5 do
     |> String.split("\n")
   end
 
-  def find_row(partition_str) do
+  def find_row(row_str) do
+    find_partition(row_str, 127)
+  end
+
+  def find_column(col_str) do
+    find_partition(col_str, 7)
+  end
+
+  defp find_partition(partition_str, high) do
     {max, min} = partition_str
                           |> String.graphemes()
-                          |> Enum.reduce( {127, 0}, fn ch, acc -> partition(ch, acc) end)
+                          |> Enum.reduce({high, 0}, fn ch, acc -> partition(ch, acc) end)
     if (max == min), do: max, else: 0
   end
   defp partition(ch, {max, min}) do
     case ch do
       "F" -> {max - ((max - min) >>> 1) - 1, min}
+      "L" -> {max - ((max - min) >>> 1) - 1, min}
       "B" -> {max, ((max - min) >>> 1) + min + 1}
+      "R" -> {max, ((max - min) >>> 1) + min + 1}
       _ -> "something went terribly wrong!"
     end
   end
@@ -44,6 +54,19 @@ defmodule Day5.BoardingPassTest do
   test "find correct row 102" do
     assert Day5.find_row("BBFFBBF") == 102
   end
+
+  test "find correct column 5" do
+    assert Day5.find_column("RLR") == 5
+  end
+
+  test "find correct column 7" do
+    assert Day5.find_column("RRR") == 7
+  end
+
+  test "find correct column 4" do
+    assert Day5.find_column("RLL") == 4
+  end
+
 end
 
 
