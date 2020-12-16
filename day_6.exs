206185e2d2b1e38c9922c26546c15b94603ae229
Stuart Osborne <stuart.osborne@infinera.com>
2020-12-16 19:16:59 +0100

    Yay! Day 6 complete...looking good but slow

--- day_6.exs
+++ day_6.exs
@@ -8,7 +8,7 @@ defmodule Day6 do
     |> Enum.map(fn group -> String.split(group, "\n") end)
   end
 
-  def sum_counts_for_groups_any_answer() do
+  def sum_counts_for_questions_anyone_answered() do
     load_group_questions()
     |> Enum.map(fn group_list -> Enum.join(group_list) end)
     |> Enum.map(fn str -> String.graphemes(str) end)
@@ -17,21 +17,30 @@ defmodule Day6 do
     |> Enum.sum()
   end
 
-end
+  def sum_counts_for_questions_everyone_answered() do
+    load_group_questions()
+    |> Enum.map(fn group -> count_questions_answered_by_everyone_in_group(group) end)
+    |> Enum.sum()
+  end
 
-# with tests
-ExUnit.start()
-defmodule Day6.Questions do
-  use ExUnit.Case
+  defp count_questions_answered_by_everyone_in_group(group) do
+    [shortest_list | rest_lists] = group
+                                   |> Enum.map(fn questions -> String.graphemes(questions) end)
+                                   |> Enum.sort(fn x, y -> length(x) <= length(y) end)
+    shortest_list
+    |> Enum.reduce(0, fn q, acc -> acc + match_group_questions(rest_lists, q) end)
+  end
 
-  test "true is true" do
-    assert true == true
+  defp match_group_questions(group_question_lists, question) do
+    question_in_all_lists = group_question_lists
+                            |> Enum.reduce(true, fn list, acc -> acc && Enum.member?(list, question) end)
+    if (question_in_all_lists == true), do: 1, else: 0
   end
 
 end
 
 #First star!!
-IO.puts(Day6.sum_counts_for_groups_any_answer())
+IO.puts(Day6.sum_counts_for_questions_anyone_answered())
 
 #Second star!!
-IO.puts("second star goes here")
+IO.puts(Day6.sum_counts_for_questions_everyone_answered())
