59673c4cefb72df3dde2f997c6f8b4be58f296ef
Stuart Osborne <stuart.osborne@infinera.com>
2020-12-29 20:56:57 +0100

    First star day 8...

--- day_8.exs
+++ day_8.exs
@@ -7,6 +7,29 @@ defmodule Day8 do
     instructions
     |> String.trim()
     |> String.split("\n")
+    |> Enum.map(fn instr -> {instr, false} end)
+  end
+
+  def parse_instruction(instruction, {acc, row}) do
+    [instr, op] = String.split(instruction, " ")
+    case instr do
+      "nop" -> {acc, row + 1}
+      "acc" -> {acc + String.to_integer(op), row + 1}
+      "jmp" -> {acc, row + String.to_integer(op)}
+    end
+  end
+
+  def run_instructions(instructions) do
+    run_instructions(instructions, {0, 0}, false)
+  end
+  def run_instructions(_flagged_instructions, {acc, _row}, true) do
+    acc
+  end
+  def run_instructions(flagged_instructions, {acc, row}, _row_state) do
+    IO.inspect(flagged_instructions)
+    {inst, flag} = Enum.at(flagged_instructions, row)
+    instructions = List.update_at(flagged_instructions, row, fn {inst, _flag} -> {inst, true} end)
+    run_instructions(instructions, parse_instruction(inst, {acc, row}), flag)
   end
 
 end
@@ -18,9 +41,9 @@ defmodule Day8.BootCodeTest do
   import Day8
 
 end
-
+instructions = Day8.read_boot_code()
 #First star!!
-
+IO.puts(Day8.run_instructions(instructions))
 
 #Second star!!
 
